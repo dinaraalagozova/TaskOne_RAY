db62372431418f47be60f54b63e9245005ef1e6b
DinaraAlagozova <dinara.alagozova@outlook.com>
2023-05-15 16:09:06 +0600

    Initial Commit

---
+++ TaskOne_RAY/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskOne_RAY
+//
+//  Created by Dinara Alagozova on 15.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
