bb35ee422a6157626ed8d89e82886264a52db57b
DinaraAlagozova <dinara.alagozova@outlook.com>
2023-05-20 16:26:16 +0600

    First init

--- TaskOne_RAY/ViewController.swift
+++ TaskOne_RAY/ViewController.swift
@@ -8,12 +8,70 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    var matrix: [[Int]] = [
+           [1, 0, 1],
+           [0, 1, 0],
+           [0, 0, 0]
+       ]
+       
+    var distances: [[Int]] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        distances = calculateDistances(matrix: matrix)
+               
+        // Выводим полученные расстояния
+        for row in distances {
+                print(row)
+            }
     }
+        
+        func calculateDistances(matrix: [[Int]]) -> [[Int]] {
+            let rows = matrix.count
+            let columns = matrix[0].count
+            
+            // Создаем новую матрицу для хранения расстояний
+            var distances = Array(repeating: Array(repeating: Int.max, count: columns), count: rows)
+            
+            // Переменная для хранения текущего расстояния
+                var currentDistance = 0
+                
+                // Проходимся по матрице и обновляем расстояние для каждой ячейки
+                for i in 0..<rows {
+                    for j in 0..<columns {
+                        if matrix[i][j] == 1 {
+                            // Если текущая ячейка содержит 1, то расстояние равно 0
+                            distances[i][j] = 0
+                            currentDistance = 0
+                        } else {
+                            // Иначе находим расстояние до ближайшей ячейки со значением 1
+                            currentDistance += 1
+                            distances[i][j] = currentDistance
+                        }
+                    }
+                    currentDistance = 0
+                }
+                
+                // Проходимся по матрице в обратном направлении и обновляем расстояния
+                for i in (0..<rows).reversed() {
+                    for j in (0..<columns).reversed() {
+                        if matrix[i][j] == 1 {
+                            // Если текущая ячейка содержит 1, то расстояние равно 0
+                            distances[i][j] = 0
+                            currentDistance = 0
+                        } else {
+                            // Иначе проверяем расстояние к соседней ячейке с минимальным расстоянием
+                            currentDistance += 1
+                            distances[i][j] = min(distances[i][j], currentDistance)
+                        }
+                    }
+                    currentDistance = 0
+                }
+            return distances
+        }
 
-
-}
-
+    }
+    
+    
+   
